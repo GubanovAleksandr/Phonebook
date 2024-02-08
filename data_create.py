ee95b9412715713b3819c90338d985313ad7d813
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2024-02-08 12:19:25 +0500

    1

---
+++ data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name =input("Введите имя: ")
+    print("Красивое!")
+    return name
+
+def surname_data():
+    surname =input("Введите Фамилию: ")
+    print("Красивое!")
+    return surname
+
+def phone_data():
+    phone =input("Введите телефон: ")
+    print("Красивое!")
+    return phone
+
+def address_data():
+    address =input("Введите адрес: ")
+    print("Красивое!")
+    return address
